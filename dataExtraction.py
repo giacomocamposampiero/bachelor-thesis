96adc8124a039af9f1e75b08ddc91b5378992364
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2021-04-19 11:03:59 +0200

    Splitted data of different types of graphs

--- dataExtraction.py
+++ dataExtraction.py
@@ -42,13 +42,15 @@ def get_elapsed_times(name, data):
 def save_results(classes = ["gnp", "bag", "rrg", "wsg"]):
     # open csv file
     writer = csv.writer(open("data.csv", 'w'))
-    # write label row
-    writer.writerow(['name', 'time', 'ticks','sol_nodes', 'gap', 'time_lim', 'nodes', 'edges', 'cnnct_cmp', 'avg_clust', 'std_dev_clust', 'radius', 'diameter'])
+    # label row
+    labels = ['name', 'time', 'ticks','sol_nodes', 'gap', 'time_lim', 'nodes', 'edges', 'cnnct_cmp', 'avg_clust', 'std_dev_clust', 'radius', 'diameter']
     resFolder = "mip-results/"
     graphFolder = "graph-instances/"
     # build a list of file names contained in the folder
     files = [f for f in listdir(resFolder) if isfile(join(resFolder, f))]
-    # for each adjacency list
+    # dictionary containing data for each type
+    data = dict((key, [labels]) for key in classes)
+    # for each graph
     for name in files:
         if name[0:3] in classes:
             id = name.split(".")[0]
@@ -61,6 +63,11 @@ def save_results(classes = ["gnp", "bag", "rrg", "wsg"]):
             row.append(lineList[-1])
             row.append(False if float(lineList[-1]) == 0.0 else True)
             row.extend(analyze_graph(graph))
+            data[name[0:3]].append(row)
+    # write each class data in a dedicated file
+    for key in classes:
+        writer = csv.writer(open("data/"+key+".csv", "w"))
+        for row in data[key]:
             writer.writerow(row)
 
 if __name__ == "__main__":
