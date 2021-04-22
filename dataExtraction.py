e4bd51129e8f83b3909801477a15c9abe5da57c0
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2021-04-22 09:42:15 +0200

    Modified data extrection process

--- dataExtraction.py
+++ dataExtraction.py
@@ -3,8 +3,6 @@ from numpy import mean
 from numpy import std
 # import graph utility library
 import networkx as nx
-# draw graph
-from pyvis.network import Network
 # import libraries for graph file reading
 from os import listdir
 from os.path import isfile, join
@@ -12,10 +10,13 @@ from os.path import isfile, join
 import re
 # import csv lib
 import csv
+# import experiment params
+import params as pa
+# cartesian product function
+from itertools import product
 
 def analyze_graph(graph):
     info = list()
-    info.append(nx.number_of_nodes(graph))
     info.append(nx.number_of_edges(graph))
     info.append(nx.number_connected_components(graph))
     clustering = nx.clustering(graph)
@@ -27,33 +28,42 @@ def analyze_graph(graph):
         rad = [-1, -1]
     info.extend(rad)
     return info
-
-def draw(graph):
-    nt = Network(notebook=True)
-    nt.barnes_hut()
-    nt.from_nx(graph)
-    nt.show('nx.html')
-
-def get_elapsed_times(name, data):
-    res = re.findall("\d+\.\d+", data)
-    res.insert(0, name)
-    return res
-
+    
+def graph_parameters(classes):
+    params = [[],[],[],[]]
+    for i in range(len(classes)):
+        if classes[i] == 'gnp':
+            params[i] = list(product(pa.GNP_N, pa.GNP_P, pa.SEEDS))
+            params[i].insert(0, ('n', 'p', 'seed'))
+        elif classes[i] == 'rrg':
+            params[i] = list(product(pa.RR_N, pa.RR_D, pa.SEEDS))
+            params[i].insert(0, ('n', 'd', 'seed'))
+        elif classes[i] == 'wsg':
+            params[i] = list(product(pa.WS_N, pa.WS_P, pa.WS_K, pa.SEEDS))
+            params[i].insert(0, ('n', 'p', 'k', 'seed'))
+        elif classes[i] == 'bag':
+            params[i] = list(product(pa.BA_N, pa.BA_M, pa.SEEDS))
+            params[i].insert(0, ('n', 'm', 'seed'))
+    return params 
+    
 def save_results(classes = ["gnp", "bag", "rrg", "wsg"]):
     # open csv file
     writers = [csv.writer(open("data/"+classes[i]+".csv", 'w')) for i in range(len(classes))]
     # label row
-    labels = ['name', 'time', 'ticks','sol_nodes', 'gap', 'time_lim', 'nodes', 'edges', 'cnnct_cmp', 'avg_clust', 'std_dev_clust', 'radius', 'diameter']
-    # add labels to each file
+    labels_st = ['name']
+    labels_rd = ['time', 'ticks','sol_nodes', 'gap', 'time_lim', 'edges', 'cnnct_cmp', 'avg_clust', 'std_dev_clust', 'radius', 'diameter']
+    # get experiment parameters
+    parameters = graph_parameters(classes)
+    # write labels in each file
     for i in range(len(classes)):
-         writers[i].writerow(labels)
+        writers[i].writerow(labels_st + list(parameters[i].pop(0)) + labels_rd)
     # useful folders path
     resFolder = "mip-results/"
     graphFolder = "graph-instances/"
     # build a list of file names contained in the folder
     files = [f for f in listdir(resFolder) if isfile(join(resFolder, f))]
     files.sort()
-    # for each graph
+    # for each graph in the folder
     for name in files:
         if name[0:3] in classes:
             id = name.split(".")[0]
@@ -61,7 +71,9 @@ def save_results(classes = ["gnp", "bag", "rrg", "wsg"]):
             lineList = fileHandle.readlines()
             fileHandle.close()
             graph = nx.read_adjlist(graphFolder + id + ".adjlist")
-            row = get_elapsed_times(id, lineList[-3])
+            row = [id]
+            row.extend(list(parameters[classes.index(id[0:3])].pop(0)))
+            row.append(re.findall("\d+\.\d+", lineList[-3]))
             row.append(lineList[-2])
             row.append(lineList[-1])
             row.append(False if float(lineList[-1]) == 0.0 else True)
@@ -69,8 +81,4 @@ def save_results(classes = ["gnp", "bag", "rrg", "wsg"]):
             writers[classes.index(name[0:3])].writerow(row)
 
 if __name__ == "__main__":
-#    print("which graph: ")
-#    name = input()
-#    graph = nx.read_adjlist("graph-instances/" + name+".adjlist")
-#    draw(graph)
-     save_results()
+    save_results()
